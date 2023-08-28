99884a4381e49207427f9d70301a30dd6448d139
Yorick <70941336+Yorick-xy@users.noreply.github.com>
2023-08-28 03:04:50 +0200

    Update iindex.php

--- iindex.php
+++ iindex.php
@@ -1,16 +1,5 @@
 <?php
 
-// Page created by Shepard [Fabian Pijcke] <Shepard8@laposte.net>
-// Arno Esterhuizen <arno.esterhuizen@gmail.com>
-// and Romain Bourdon <rromain@romainbourdon.com>
-// and Hervé Leclerc <herve.leclerc@alterway.fr>
-// Icons by Mark James <http://www.famfamfam.com/lab/icons/silk/>
-// Version 2.5 -> 3.0.0 by Dominique Ottello aka Otomatic
-// 3.1.9 - Support VirtualHost IDNA ServerName
-//
-//
-//
-
 $server_dir = "../";
 
 require $server_dir.'scripts/config.inc.php';
